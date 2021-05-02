0b4d1dc1f93d3add44dcaab8a1d51d5d994cd959
salvador <josesalvadorpp@hotmail.com>
2021-05-02 16:51:33 -0400

    calculate FPS

--- src/init.js
+++ src/init.js
@@ -11,13 +11,35 @@ ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
 // Start the first frame request
 window.requestAnimationFrame(gameLoop);
 
+//global variables
+let secondsPassed;
+let oldTimeStamp;
+let fps;
+
 function gameLoop(timeStamp) {
 	clearScreen();
 	//game's logic in main function
 	main();
 
+	//calculate Fps
+	calculateFPS(timeStamp);
+
 	// Keep requesting new frames
 	window.requestAnimationFrame(gameLoop);
+}
+
+function calculateFPS(timeStamp) {
+	// Calculate the number of seconds passed since the last frame
+	secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+	oldTimeStamp = timeStamp;
+
+	// Calculate fps
+	fps = Math.round(1 / secondsPassed);
+
+	// Draw number to the screen
+	ctx.font = '25px Arial';
+	ctx.fillStyle = 'white';
+	ctx.fillText('FPS: ' + fps, 10, 30);
 }
 
 function clearScreen() {
